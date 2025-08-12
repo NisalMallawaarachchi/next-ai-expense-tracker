49f120df04d0e0fe217c58608c3c00bbd8067cff
NisalMallawaarachchi <nisalmallawarachchi@gmail.com>
2025-08-12 16:59:41 +0530

    add Clerk Authentication

---
+++ lib/checkUser.ts
@@ -0,0 +1,30 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { db } from "@/lib/db";
+
+export async function checkUser() {
+  const user = await currentUser();
+  if (!user) {
+    return null;
+  }
+
+  const loggedInUser = await db.user.findUnique({
+    where: {
+      clerkUserId: user.id,
+    },
+  });
+
+  if (loggedInUser) {
+    return loggedInUser; // ✅ Return existing user
+  }
+
+  const newUser = await db.user.create({
+    data: {
+      clerkUserId: user.id,
+      name: `${user.firstName ?? ""} ${user.lastName ?? ""}`.trim(),
+      imageUrl: user.imageUrl,
+      email: user.emailAddresses[0]?.emailAddress || "",
+    },
+  });
+
+  return newUser;
+}
